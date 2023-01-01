7a178c472c275a0f1491e1a41382f6b19c9db4ea
saymon-shoab <mdsaymonshoab@gmail.com>
2023-01-01 10:44:57 +0600

    input ouotput system

---
+++ week1/module1/inputoutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    // single input single output
+    cin >> x;
+    cout << x; 
+    // multiple input multiple output
+    int a,b;
+    cin >>a>>b;
+    cout<<a<<b;
+    return 0;
+}
